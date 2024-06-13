ae6d233b40a697cc9e2ebf7509d4d702c33c6cef
Vaxo Tivadze <vtivadze@yahoo.com>
2024-06-13 23:15:25 +0400

    fixed item handler logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,10 +13,12 @@ const Cart = (props) => {
 
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
+
   };
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
+    
   };
 
   const cartItems = (
