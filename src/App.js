54491ecf6f9ed71c0e5eb90cda0c6cb7f5962dac
Vakhtang Tivadze <vaxotivadze@gmail.com>
2024-06-13 22:47:41 +0400

    merged cart feature 2

--- src/App.js
+++ src/App.js
@@ -18,13 +18,8 @@ function App() {
 
   return (
     <CartProvider>
-<<<<<<< HEAD
-      {cartIsShown && <div>Cart...</div>}
+      {cartIsShown && <Cart  onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
-=======
-      <Cart />
-      <Header />
->>>>>>> feat/cart
       <main>
         <Meals />
       </main>
